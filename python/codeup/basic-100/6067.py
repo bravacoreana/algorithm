efecc0023fa6d1fd8a5959d64e9554ad1d3dbd58
bravacoreana <bravacoreana@gmail.com>
2021-09-16 17:30:08 +0200

    add codeup python basic 6067

---
+++ python/codeup/basic-100/6067.py
@@ -0,0 +1,13 @@
+
+x=int(input())
+
+if x < 0 :
+    if x % 2 == 0 : 
+        print("A")
+    else :
+        print("B")
+else :
+    if x % 2 == 0 :
+        print("C")
+    else :
+        print("D")
