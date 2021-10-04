cdae7c821c180865297ebbbc15f1d8e2cb18da61
Liha Kim <bravacoreana@gmail.com>
2021-10-04 08:04:31 +0200

    fix timeout codeup python basic 6098

--- python/codeup/basic-100/part9/6098.py
+++ python/codeup/basic-100/part9/6098.py
@@ -1,6 +1,32 @@
-board = [
-    [0 for i in range(10)] for j in range(10)
-]
+################# TIME OUT CODE #################
+# board = [[0 for i in range(10)] for j in range(10)]
+
+
+# for i in range(10) :
+#     point = input().split()
+#     board[i] = list(map(int,point))
+
+# x,y = 1,1
+
+# while True:
+#     if(board[x][y] == 0) :
+#         board[x][y] = 9
+#         if board[x][y+1] == 0 :
+#             y += 1
+#         else :
+#             x += 1
+#     if board[x][y] == 2 :
+#         board[x][y] = 9
+#         break
+
+# for i in range(10) : 
+#     for j in range(10) : 
+#         print(board[i][j], end=' ') 
+#     print()
+
+################# TIME OUT CODE END #################
+
+board = [[0 for i in range(10)] for j in range(10)]
 
 
 for i in range(10) :
@@ -9,19 +35,15 @@ for i in range(10) :
 
 x,y = 1,1
 
-while True:
-    if(board[x][y] == 0) :
-        board[x][y] = 9
-        if board[x][y+1] == 0 :
-            y += 1
-        else :
-            x += 1
+while board[x][y] == 0 :
+    board[x][y] = 9
+    if board[x][y+1] == 0 or board[x][y+1] == 2 :
+        y += 1
+    else :
+        x += 1
     if board[x][y] == 2 :
         board[x][y] = 9
-        break
 
-
-print()
 for i in range(10) : 
     for j in range(10) : 
         print(board[i][j], end=' ') 
