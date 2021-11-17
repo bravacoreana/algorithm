774fc3ccb81f62af48d3f6e6f20df70310962e55
Liha Kim <bravacoreana@gmail.com>
2021-11-17 21:53:28 +0100

    Add hackerrank: lonely-integer

---
+++ python/hackerrank/lonely-integer/lonely-integer.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    b=[]
+    c=[]
+    for i in a:
+       if i not in b:
+            b.append(i) 
+       else:
+            c.append(i)
+    for i in b:
+        if i not in c:
+            return i
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
